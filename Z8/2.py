f2193ada49a1f3eca1a62ac3654947c52440a85f
NIKITAPLITNIK <144878650+NIKITAPLITNIK@users.noreply.github.com>
2023-12-05 00:50:21 +0300

    Delete Z8 directory

--- Z8/2.py
+++
@@ -1,8 +0,0 @@
-# вариант 2
-def calculateAreaOfRectangle(a: float, b: float) -> float:
-    return a * b
-
-firstRectangle: float = calculateAreaOfRectangle(float(input()), float(input()))
-secondRectangle: float = calculateAreaOfRectangle(float(input()), float(input()))
-thirdRectangle: float = calculateAreaOfRectangle(float(input()), float(input()))
-print(f"{firstRectangle}, {secondRectangle}, {thirdRectangle}")
