e1c8166e1fc9e1e9ef2b1a1d6d2a9edb6971b9d0
NIKITAPLITNIK <144878650+NIKITAPLITNIK@users.noreply.github.com>
2023-12-05 00:50:33 +0300

    Delete Z6 directory

--- Z6/1.py
+++
@@ -1,7 +0,0 @@
-# вариант 2
-def replaceChars(string: str, replacedChar: str = ':', newChar: str = '%') -> None:
-    newString: str = string.replace(replacedChar, newChar)
-    print(f"{newString}\n{newString.count(newChar)}")
-
-message: str = ".::_T:T_::."
-replaceChars(message)
