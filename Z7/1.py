ebeb8e54519e6fab7be1c47afbda44ff11bd3c94
NIKITAPLITNIK <144878650+NIKITAPLITNIK@users.noreply.github.com>
2023-12-05 00:50:27 +0300

    Delete Z7 directory

--- Z7/1.py
+++
@@ -1,9 +0,0 @@
-# вариант 2
-def createUserArray(N: int) -> list:
-    array: list = []
-    for i in range(N):
-        array.append(int(input()))
-    return array
-
-userArray: list = createUserArray(5)
-print(userArray.index(min(userArray)))
